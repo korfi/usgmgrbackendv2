62997bfb202ef9e82088a3a9872915fe5c27ad8a
Krzysiek <krzysiek@orfinger.com>
2017-04-03 09:20:29 +0200

    no latency working version v1

---
+++ USG_backend_console/GlobalSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace USG_backend_console
+{
+    static class GlobalSettings
+    {
+        public static TcpListener serverSocket = null;
+        public static TcpClient clientSocket = null;
+    }
+}
--- USG_backend_console/ServerRedirector.cs
+++ USG_backend_console/ServerRedirector.cs
@@ -37,45 +37,13 @@ namespace USG_backend_console
                 GlobalSettings.clientSocket = GlobalSettings.serverSocket.AcceptTcpClient();
                 Console.WriteLine("Connected!\r\n");
 
-                //int i;
-
-                //int commandCharSize = 4;  
                 NetworkStream stream = GlobalSettings.clientSocket.GetStream();
                 StreamReader reader = new StreamReader(stream);
                 while (GlobalSettings.clientSocket.Connected)
                 {
                     try {
-                        //string dataLeft = "";
-                        //while (reader.Peek() < 0)
-                        //{
-                            //Thread.Sleep(5);
-                        //};
-                        /*while (reader.Peek() >= 0)
-                        {
-                            dataLeft += (char)reader.Read();
-                        }
-                        Console.WriteLine("Stream read");
-                        // Translate data bytes to a ASCII string.
-                        Console.WriteLine(String.Format("Received: {0}", dataLeft));
-
-                        int currDataPos = 0;
-                        while (dataLeft.Length >= commandCharSize)
-                        {
-                            string currData = dataLeft.Substring(currDataPos, commandCharSize);
-                            currDataPos += commandCharSize;
-                            dataLeft = currData.Substring(currDataPos, dataLeft.Length-currDataPos);*/
-                            string currData = reader.ReadLine();
-                            ah.HandleStringCommand(currData, GlobalSettings.clientSocket.Client.RemoteEndPoint.ToString());
-
-                            // Process the data sent by the client.
-                            //currData = currData.ToUpper();
-
-                            //byte[] msg = System.Text.Encoding.ASCII.GetBytes(currData);
-
-                            // Send back a response.
-                            //stream.Write(msg, 0, msg.Length);
-                            //Console.WriteLine(String.Format("Sent: {0}", currData));
-                        //}
+                       string currData = reader.ReadLine();
+                       ah.HandleStringCommand(currData, GlobalSettings.clientSocket.Client.RemoteEndPoint.ToString());
                     }
                     catch (Exception ex) { 
                         Console.WriteLine("Couldn't process sending/receiving data: " + ex.Message);
