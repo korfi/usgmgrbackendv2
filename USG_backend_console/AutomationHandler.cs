ce874b3952b37c6dbdf45e19865cc383fe9b2f3d
Krzysiek <krzysiek@orfinger.com>
2017-06-05 08:25:25 +0200

    implemented TCG controls

--- USG_backend_console/AutomationHandler.cs
+++ USG_backend_console/AutomationHandler.cs
@@ -55,6 +55,70 @@ namespace USG_backend_console
                     Console.WriteLine("Pressing save button");
                     saveBtnPress();
                     break;
+                case "tg1u":
+                    Console.WriteLine("Pressing TCG1 Up");
+                    tgc1upBtnPress();
+                    break;
+                case "tg1d":
+                    Console.WriteLine("Pressing TCG1 Down");
+                    tgc1downBtnPress();
+                    break;
+                case "tg2u":
+                    Console.WriteLine("Pressing TCG2 Up");
+                    tgc2upBtnPress();
+                    break;
+                case "tg2d":
+                    Console.WriteLine("Pressing TCG2 Down");
+                    tgc2downBtnPress();
+                    break;
+                case "tg3u":
+                    Console.WriteLine("Pressing TCG3 Up");
+                    tgc3upBtnPress();
+                    break;
+                case "tg3d":
+                    Console.WriteLine("Pressing TCG3 Down");
+                    tgc3downBtnPress();
+                    break;
+                case "tg4u":
+                    Console.WriteLine("Pressing TCG4 Up");
+                    tgc4upBtnPress();
+                    break;
+                case "tg4d":
+                    Console.WriteLine("Pressing TCG4 Down");
+                    tgc4downBtnPress();
+                    break;
+                case "tg5u":
+                    Console.WriteLine("Pressing TCG5 Up");
+                    tgc5upBtnPress();
+                    break;
+                case "tg5d":
+                    Console.WriteLine("Pressing TCG5 Down");
+                    tgc5downBtnPress();
+                    break;
+                case "tg6u":
+                    Console.WriteLine("Pressing TCG6 Up");
+                    tgc6upBtnPress();
+                    break;
+                case "tg6d":
+                    Console.WriteLine("Pressing TCG6 Down");
+                    tgc6downBtnPress();
+                    break;
+                case "tg7u":
+                    Console.WriteLine("Pressing TCG7 Up");
+                    tgc7upBtnPress();
+                    break;
+                case "tg7d":
+                    Console.WriteLine("Pressing TCG7 Down");
+                    tgc7downBtnPress();
+                    break;
+                case "tg8u":
+                    Console.WriteLine("Pressing TCG8 Up");
+                    tgc8upBtnPress();
+                    break;
+                case "tg8d":
+                    Console.WriteLine("Pressing TCG8 Down");
+                    tgc8downBtnPress();
+                    break;
                 case "8bgr":
                     GlobalSettings.writer.WriteLine("Changing palette to 8-bit greyscale");
                     paletteChange("8-bit linear grayscale");
@@ -131,6 +195,133 @@ namespace USG_backend_console
         private void areaDownBtnPress()
         {
             System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "area_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc1upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC1_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc1downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC1_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc2upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC2_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc2downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC2_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc3upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC3_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc3downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC3_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc4upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC4_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc4downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC4_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+        private void tgc5upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC5_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc5downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC5_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc6upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC6_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc6downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC6_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc7upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC7_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc7downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC7_down");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc8upBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC8_up");
+            AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
+            InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            valuetextOne.Invoke();
+        }
+
+        private void tgc8downBtnPress()
+        {
+            System.Windows.Automation.Condition textConditionOne = new PropertyCondition(AutomationElement.AutomationIdProperty, "TGC8_down");
             AutomationElement textOne = testWindow.FindFirst(TreeScope.Descendants, textConditionOne);
             InvokePattern valuetextOne = textOne.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
             valuetextOne.Invoke();
